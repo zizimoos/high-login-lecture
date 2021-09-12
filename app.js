a08fab8bc0c928bb0aad0feaefddc9be8e5490d8
henry <henry@iMac-home.local>
2021-09-12 15:10:34 +0900

    express static front js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);
 
 export default app;
