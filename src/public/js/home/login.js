4de1b8158c2f365828eac5abd2d38bf93820a58a
henry <henry@iMac-home.local>
2021-09-12 15:54:38 +0900

    로그인 요청 fetch 생성

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -1,1 +1,20 @@
-console.log(`this is login`);
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const logButton = document.querySelector("button");
+
+const login = () => {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req, JSON.stringify(req));
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+};
+logButton.addEventListener("click", login);
